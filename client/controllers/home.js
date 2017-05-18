66d88880266e0e59a29ef0a757969d5e8ba97ba2
XxShadowWolfxX <alim.0158@gmail.com>
2017-05-18 16:40:35 -0400

    Parts 7 & 8 Completed

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,12 +1,16 @@
 angular.module("Instagram")
-  .controller("HomeCtrl", function($scope) {
+  .controller('HomeCtrl', function($scope, $window, $rootScope, $auth) {
 
-    $scope.isAuthenticated = function() {
-      // check if logged in
+	$scope.isAuthenticated = function() {
+      return $auth.isAuthenticated();
     };
 
     $scope.linkInstagram = function() {
-      // connect email account with instagram
-    };
-
-  });
+      $auth.link('instagram')
+      	.then(function(response) {
+      		$window.localStorage.currentUser = JSON.stringify(response.data.user);
+      		$rootScore.currentUser = JSON.parse($window.localStorage.currentUser);
+      	});
+  };
+  
+ });
---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	db: 'localhost',
+	clientSecret: process.env.clientSecret || 'c8fa6489ef9e41859db4a218b710efdb',
+	tokenSecret: process.enc.clientSecret || 'I do not know what I am doing so meh'
+};
